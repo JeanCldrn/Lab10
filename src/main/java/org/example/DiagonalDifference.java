3d68482494345083f9d3e30f4b1600c60556a970
JeanCldrn <jeancarlocldrn@gmail.com>
2023-06-21 11:20:25 -0600

    -First exercise

---
+++ src/main/java/org/example/DiagonalDifference.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class DiagonalDifference {
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int result=0;
+        int j =arr.size()-1;
+        for(int i=0;i<arr.size(); i++){
+            result+=arr.get(i).get(i);
+            result-= arr.get(j-i).get(i);
+
+        }
+        return Math.abs(result);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
